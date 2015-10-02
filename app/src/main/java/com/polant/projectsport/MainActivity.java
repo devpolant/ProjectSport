68b63270758a9bf0b755ca6d8e935c390791f6cd
Anton <anton.poltoratskii@gmail.com>
2015-10-02 14:57:46 +0300

    Add NavigationView model

--- app/src/main/java/com/polant/projectsport/MainActivity.java
+++ app/src/main/java/com/polant/projectsport/MainActivity.java
@@ -2,15 +2,38 @@ package com.polant.projectsport;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Антон on 02.10.2015.
  */
 public class MainActivity extends AppCompatActivity {
 
+    public static final int LAYOUT = R.layout.activity_main;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
+        setContentView(LAYOUT);
+
+        initToolbar();
     }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
 }
