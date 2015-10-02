b2994070df5b642746f73eff8152800c8c693373
Anton <anton.poltoratskii@gmail.com>
2015-10-02 14:23:09 +0300

    Initial commit

---
+++ app/src/main/java/com/polant/projectsport/MainActivity.java
@@ -0,0 +1,16 @@
+package com.polant.projectsport;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Антон on 02.10.2015.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
