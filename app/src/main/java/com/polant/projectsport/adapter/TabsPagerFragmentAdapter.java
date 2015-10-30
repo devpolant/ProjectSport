cec1ba9fe1d924d96117b7977c1ea5715141efb2
Anton <anton.poltoratskii@gmail.com>
2015-10-30 23:08:09 +0200

    delete ExampleFragment and his layout

--- app/src/main/java/com/polant/projectsport/adapter/TabsPagerFragmentAdapter.java
+++ app/src/main/java/com/polant/projectsport/adapter/TabsPagerFragmentAdapter.java
@@ -1,21 +1,14 @@
 package com.polant.projectsport.adapter;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 import com.polant.projectsport.R;
-import com.polant.projectsport.data.model.Article;
-import com.polant.projectsport.fragment.ExampleFragment;
 import com.polant.projectsport.fragment.article.ArticleFragment;
-import com.polant.projectsport.fragment.chart.ChartStatisticsFragment;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * Created by Антон on 02.10.2015.
